995b64e199af1d42132a356013bdf4492a5ca9ea
João Spengler <desenvolvimento9@softin.com.br>
2018-08-23 12:59:16 -0300

    first commit

---
+++ Uri1611/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uri1611
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCase = Convert.ToInt32(Console.ReadLine());
+
+            for (int y = 0; y < testCase; y++)
+            {
+                string firstLine = Console.ReadLine();
+                string[] variables = firstLine.Split(' ');
+                int totalFloors = Convert.ToInt32(variables[0]);
+                int capacity = Convert.ToInt32(variables[1]);
+                int group = Convert.ToInt32(variables[2]);
+
+                string floors = Console.ReadLine();
+                string[] visitFloors = floors.Split(' ');
+                List<string> floorsList = new List<string>();
+                floorsList.AddRange(visitFloors);
+                //List<int> numberFloor = floorsList.ConvertAll(new Converter<string, int>(StringToInt));
+                List<int> numberFloor = floorsList.Select(s => Convert.ToInt32(s)).ToList();
+                numberFloor.Sort();
+                numberFloor.Reverse();
+
+                bool pass = true;
+
+                int maxFloor = 0;
+                for (int i = 0; i < group; i++) //OK
+                {
+                    if (numberFloor[i] > maxFloor)
+                    {
+                        maxFloor = numberFloor[i];
+                    }
+                    if (maxFloor > totalFloors)
+                    {
+                        Console.WriteLine("Invalid Floor, out of range");
+                        Console.ReadLine();
+                        pass = false;
+                        break;
+                    }
+                }
+
+                //DisplayListFloor(floorsList);
+                //DisplayNumberFloor(numberFloor);
+
+                int energy = 0;
+                if (pass == true)
+                {
+                    int remove = 0;
+                    while (numberFloor.Count != 0)
+                    {
+                        int energyBase = numberFloor[0];
+                        if (numberFloor.Count >= capacity)
+                        {
+                            numberFloor.RemoveRange(remove, capacity);
+                        }
+                        else
+                        {
+                            numberFloor.RemoveRange(remove, numberFloor.Count);
+                        }
+                        energy += (energyBase * 2);
+                    }
+                    DisplayEnergy(energy);
+                }
+            }
+        }
+
+        private static void DisplayEnergy(int show)
+        {
+            Console.WriteLine(show);
+        }
+        private static void DisplayListFloor(List<string> floorsList)
+        {
+            Console.WriteLine();
+            foreach (string floor in floorsList)
+            {
+                Console.WriteLine(floor);
+            }
+        }
+        private static void DisplayNumberFloor(List<int> numberFloor)
+        {
+            Console.WriteLine();
+            foreach (int floor in numberFloor)
+            {
+                Console.WriteLine(floor);
+            }
+        }
+    }
+}
---
+++ Uri1611/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Uri1611")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Uri1611")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("db43d68a-9011-41aa-abfd-a9ad4a1dd7e7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
